a3f7aee28bcc0e504e0bc9db96060a804fc99f09
ReeshU Raj <reeshu54raj@gmail.com>
2021-08-12 12:03:03 +0530

    ignore check

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/searchme.tech';
+$config['base_url'] = 'http://localhost/git_searchme/searchme.techs'; 
 
 /*
 |--------------------------------------------------------------------------
